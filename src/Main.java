f385bb3fa39fb8e1f37f17daed081d87b22893f2
Andrey Malov <maa.pers@gmail.com>
2022-04-25 19:26:30 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketAmount = 40000;
+        int mileQuote = 20;
+        int bonusMiles = ticketAmount / mileQuote;
+        System.out.println("Бонусных милей: " + bonusMiles);
+    }
+}
